dbd7e93c45d440404989acd1159db958e4ccce2e
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-09-14 21:39:16 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {React} from "react"
 import { BrowserRouter as Router,  Routes,Route } from "react-router-dom";
 
-import Home from "./home/home";
+import Home from  "./home/home";
 
 
 function App () {
